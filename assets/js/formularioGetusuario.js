f1d787fe86b6bc56542562c572204ceaeac81fcf
Ana Karen Escobar Cruz <101671249+AnnaCrupz@users.noreply.github.com>
2022-12-22 17:33:35 -0600

    fetch

--- assets/js/formularioGetusuario.js
+++ assets/js/formularioGetusuario.js
@@ -1,26 +1,35 @@
 const d=document;
 const formularioGET=d.getElementById('formGetUsuario');
-var id=25;
-const urlGet=`https://pokeapi.co/api/v2/berry/${id}/`;
+var id=1;
+const urlGet=`http://localhost:8080/usuarios/${id}`;
 const nameUsuario=d.getElementById('nameUser');
 const nombreCompleto=d.getElementById('NombreCompleto');
+const email=d.getElementById('email');
+const contrasenia=d.getElementById('password');
+const domicilio=d.getElementById('Domicilio');
+const cp=d.getElementById('cp');
+const tel=d.getElementById('tel');
+const password=d.getElementById('password')
 
-function metodoGet(urlGet) {
+
     fetch(urlGet,{
-        method:'GET',
-        headers:{
+        withCredentials: true,  
+        crossorigin: true,  
+        mode: 'no-cors',     
+        headers: {
             'Content-Type':'application/json',
-        }
-    })
-    .then((response)=> response.json())
-    .then((data)=>{
-        d.textContent=data.firmness;
-        nameUsuario.textContent=String(data.firmness.name);
-        nombreCompleto.value=data.firmness.name;
+            'Access-Control-Allow-Origin': '*'
+        },
+        method:'GET',
+
+    }).then((response)=> response.json()).then((data)=>{
+        nameUsuario.textContent=String(data.nombreUsuario);
+        console.log(data.nombreUsuario);
+        nombreCompleto.value=String(data.nombre);
     })
     .catch(()=>{
-        console.error("Error: ",error);
+        //console.error("Error: ",error);
     });
-}
 
-metodoGet(urlGet);
+
+
